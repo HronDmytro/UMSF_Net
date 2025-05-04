99fb51ef74d19b707f86a24824a83cb36bfa745c
Гронь Дмитро <unvmj5105775@gmail.com>
2025-05-04 17:47:26 +0300

    HW-06

---
+++ HW-06/WcfServiceLibrary1/WcfHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WcfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var host = new ServiceHost(typeof(WcfServiceLibrary1.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Хост запущен");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HW-06/WcfServiceLibrary1/WcfServiceLibrary1/IService1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Add(int a, int b);
+
+        // TODO: Добавьте здесь операции служб
+    }
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    // В проект можно добавлять XSD-файлы. После построения проекта вы можете напрямую использовать в нем определенные типы данных с пространством имен "WcfServiceLibrary1.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
